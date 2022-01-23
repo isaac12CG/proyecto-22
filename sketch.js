ff39930084550edf902e6a6d1a63a7785d84bde7
isaac12CG <93744143+isaac12CG@users.noreply.github.com>
2022-01-22 22:21:43 -0600

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,66 @@
+var starImg,bgImg;
+var star, starBody;
+var hada, fairyImg, fairyVoice;
+//crea la variable para el sprite del hada y fairyImg
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+function preload()
+{
+	starImg = loadImage("images/star.png");
+	bgImg = loadImage("images/starNight.png");
+	fairyImg = loadAnimation("images/fairyImage1.png","images/fairyImage2.png");
+	fairyVoice = loadSound("JoyMusic.mp3");
+}
+
+function setup() {
+	createCanvas(800, 750);
+
+	fairyImg = loadAnimation("images/fairyImage1.png","images/fairyImage2.png");
+	fairyVoice = loadSound("JoyMusic.mp3");
+
+
+	star = createSprite(650,30);
+	star.addImage(starImg);
+	star.scale = 0.2;
+
+
+	engine = Engine.create();
+	world = engine.world;
+
+	starBody = Bodies.circle(650 , 30 , 5 , {restitution:0.5, isStatic:true});
+	World.add(world, starBody);
+	
+	Engine.run(engine);
+
+}
+
+
+function draw() {
+  background(bgImg);
+
+  star.x= starBody.position.x 
+  star.y= starBody.position.y 
+
+  console.log(star.y);
+
+  if(star.y > 470 && starBody.position.y > 470) {
+	  Matter.Body.setStatic(starBody,true);
+  }
+
+  drawSprites();
+
+}
+
+function keyPressed() {
+
+	if (keyCode === DOWN_ARROW) {
+		Matter.Body.setStatic(starBody,false); 
+	}
+
+	//escribe el código para mover al hada a la izquierda y derecha
+	
+}
